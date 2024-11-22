34c0c21c06b0a2ec0ea6479c6448424305a46d90
Sruthi <147297242+Sruthi-Rajendran@users.noreply.github.com>
2024-11-22 10:13:22 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
 public class hello {
     public static void main(String[] args) {
         // Declare two numbers
-        int num1 = 25;
-        int num2 = 25;
+        int num1 = 50;
+        int num2 = 50;
 
         // Perform addition
         int sum = num1 + num2;
